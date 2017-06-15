db889f7ef7cb68bf2935d133bffd892599e3ecaa
vincent775 <775131501@qq.com>
2017-06-15 08:04:19 +0800

    Update 梯度下降法.py

--- 梯度下降法.py
+++ 梯度下降法.py
@@ -1,6 +1,8 @@
 # 本节主要讲解使用层级构造函数
+# 本节主要讲解使用层级构造函数
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 #构造层级函数
 def add_layer(inputs,in_size,out_size,activation_function=None):
@@ -25,9 +27,9 @@ xs = tf.placeholder(tf.float32,[None,1])
 ys = tf.placeholder(tf.float32,[None,1])
 
 
-# 输入层
+# 输入层 参数10是隐藏层
 l1 = add_layer(xs,1,10,activation_function=tf.nn.relu)
-# 输出层
+# 输出层 参数10是隐藏层
 predition = add_layer(l1,10,1,activation_function=None)
 
 # 校验偏差值
@@ -40,7 +42,22 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
+#数据可视化
+#定义一张图表
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1) #
+ax.scatter(x_data,y_data)
+plt.ion()
+plt.show()
+
 for i in range(1000):
     sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
     if i % 50 ==0:
-        print (sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+        #print (sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        predition_value = sess.run(predition,feed_dict={xs:x_data})
+        lines = ax.plot(x_data,predition_value,'r-',lw=5)
+        plt.pause(0.1)
